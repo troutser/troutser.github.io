4940eeed5440859ee27cd71bded129338e5b31d0
troutser <LargeFargeMan@gmail.com>
2025-04-23 17:27:45 -0700

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/troutser.github.io/', // <- replace with your actual GitHub repo name
+  build: {
+    outDir: 'docs', // ← Important
+  },
 })
